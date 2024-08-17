f2e3bd2d46aadb7d5f16232dbc34c4acc32a5d8d
Alexcheto <56219925+AlexChalakov@users.noreply.github.com>
2024-08-17 03:05:30 +0300

    New Results + 12 hour pipeline data recorded

--- configurations.py
+++ configurations.py
@@ -11,12 +11,15 @@ LOGGER = logging.getLogger('crypto_rl_log')
 MAX_BOOK_ROWS = 20  # Number of levels in the order book to capture
 DATA_PATH = "data/data_pipeline"  # Updated path to save data files
 TIMEZONE = datetime.timezone.utc  # Set timezone to UTC for consistency
-EXCHANGE = ccxt.binance()  # Binance exchange for fetching order book data
+EXCHANGE = ccxt.binance({
+    'timeout': 30000,  # 30 seconds
+    'enableRateLimit': True,
+})  # Binance exchange for fetching order book data
 EMA_ALPHA = 0.99  # Alpha value for EMA
 INTERVAL = 1  # Interval in seconds between LOB snapshots
 
 # UPDATE TO RECORD A FULL 24-HOUR DAY OF DATA
-DURATION = 24 * 60 * 60 # Total duration in seconds for data collection
+DURATION = 12 * 60 * 60 # Total duration in seconds for data collection
 # DURATION = 180  # Total duration in seconds for data collection
 
 # ./indicators/*
